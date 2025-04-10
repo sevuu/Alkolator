2dc970280c5173befe8cff66a263e8f2c4e174cb
Rodi Hasan <rodihasan1@gmail.com>
2025-04-10 22:42:00 +0200

    Minor changes

--- Alkolator/DetailsPage.xaml.cs
+++ Alkolator/DetailsPage.xaml.cs
@@ -17,14 +17,14 @@ namespace Alkolator
             lblName.Text = _beverage.Name;
             lblMl.Text = $"{_beverage.Volume} ml";
             lblAbv.Text = $"{_beverage.ABV}%";
-            lblPrice.Text = $"{_beverage.Price:C}";
-            lblAmount.Text = _beverage.Amount.ToString();
-            lblEthanol.Text = $"{_beverage.Ethanol} g";
+            lblPrice.Text = $"{_beverage.Price} zł";
+            //lblAmount.Text = _beverage.Amount.ToString();
+            lblEthanol.Text = $"{Math.Round((beverage.Ethanol / beverage.Amount), 1)} g";
             lblSpejson.Text = $"{_beverage.Spejson}";
             lblDrunk.Text = $"{Math.Round(150/(beverage.Ethanol/beverage.Amount),1)}";    // 150ML JEST WPISANE NA SZTYWNO!!! TODO: dorobić obliczanie tego
 
             ratingSlider.Value = _beverage.Rating;
-            lblRating.Text = $"Ocena: {_beverage.Rating}";
+            lblRating.Text = $"{_beverage.Rating}";
         }
 
 
@@ -32,7 +32,7 @@ namespace Alkolator
         private void OnRatingChanged(object sender, ValueChangedEventArgs e)
         {
             int rating = (int)e.NewValue;
-            lblRating.Text = $"Ocena: {rating}";
+            lblRating.Text = $"{rating}";
         }
 
         private async void OnSaveRatingClicked(object sender, EventArgs e)
